0aa84e8849543002d4a58071dce8144e489f2c26
Lorenzo V <lorenzo.vella@live.com>
2020-06-28 13:38:25 -0300

    Templates directory location added

--- Manada/settings.py
+++ Manada/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Manada.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS':  [os.path.join(BASE_DIR, "templates"), os.path.join(PROJECT_ROOT, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
