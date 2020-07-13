693bf3d4d2aaec92d80eaf47e1592fee7cdddf4e
Lorenzo V <lorenzo.vella@live.com>
2020-07-13 18:14:07 -0300

    ta loco

--- Manada/pedidos/telegramHandler.py
+++ Manada/pedidos/telegramHandler.py
@@ -5,5 +5,5 @@ def sendMessage(body, id):
     config = imgkit.config(wkhtmltoimage=('bin/wkhtmltoimage'))
     options = {'format':'jpg'}
     img = imgkit.from_url("http://www.manadafoodhouse.com.br/carrinho/detail/"+id, 'img-'+id+'.jpg', options=options, config=config)
-    with open('img-'+id+'.png', "rb") as f:
+    with open('img-'+id+'.jpg', "rb") as f:
         telegram_send.send(conf="telegramConf",messages=[body], images=[f])
