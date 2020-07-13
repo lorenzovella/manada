cbef379efd0689012eeafa60f0853298941cec14
Lorenzo V <lorenzo.vella@live.com>
2020-07-13 11:50:18 -0300

    timezone fix

--- Manada/settings.py
+++ Manada/settings.py
@@ -106,12 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
