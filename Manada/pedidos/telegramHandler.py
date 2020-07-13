46b5cfc3497b8bf6510b20bb6fff3491ce15efb1
Lorenzo V <lorenzo.vella@live.com>
2020-07-13 17:40:53 -0300

    Update telegramHandler.py

--- Manada/pedidos/telegramHandler.py
+++ Manada/pedidos/telegramHandler.py
@@ -2,7 +2,6 @@ from os import environ
 import telegram_send
 import imgkit
 def sendMessage(body, id):
-    config = imgkit.config('bin/wkhtmltopdf-linux-amd64')
     options = {'format':'jpg'}
     img = imgkit.from_url("http://www.manadafoodhouse.com/carrinho/detail/"+id, 'img-'+id+'.jpg', options=options, config=config)
     with open('img-'+id+'.png', "rb") as f:
